3275d8528a54b1a6cf44bcce5caa72519aa3c01d
mxiongmiao <mxiongmiao@gmail.com>
2013-02-27 20:36:55 +0800

    format java code use the new .vimrc

--- src/main/java/com/maxm/account/service/AccountService.java
+++ src/main/java/com/maxm/account/service/AccountService.java
@@ -9,43 +9,43 @@ import com.maxm.account.util.ErrorCode;
 
 
 public class AccountService {
-	
-	private AccountDao accountDao;
-	
-	public Map<Integer, String> addAccount(Account account) {
-		Map<Integer, String> ret = new HashMap<Integer, String>();
-		String name = account.getName();
-		if (duplicate(name)) {
-			ret.put(ErrorCode.ACCOUNTNAMEDUPLICATE, "重复的账户名");
-		} else {
-			accountDao.insertAccount(account);
-		}
-		return ret;
-	}
 
-	private boolean duplicate(String name) {
-		Account accountByName = accountDao.getAccountByName(name);
-		if (accountByName != null) {
-			return true;
-		}
-		return false;
-	}
+    private AccountDao accountDao;
 
-	public void deleteAccount(int id){
-		accountDao.deleteAccount(id);
-	}
-	
-	public void updateAccount(Account account){
-		accountDao.updateAccount(account);
-	}
-	
-	public Account getAccount(int id) {
-		Account account = accountDao.getAccountById(id);
-		return account;
-	}
+    public Map<Integer, String> addAccount(Account account) {
+        Map<Integer, String> ret = new HashMap<Integer, String>();
+        String name = account.getName();
+        if (duplicate(name)) {
+            ret.put(ErrorCode.ACCOUNTNAMEDUPLICATE, "重复的账户名");
+        } else {
+            accountDao.insertAccount(account);
+        }
+        return ret;
+    }
 
-	public void setAccountDao(AccountDao accountDao) {
-		this.accountDao = accountDao;
-	}
-	
+    private boolean duplicate(String name) {
+        Account accountByName = accountDao.getAccountByName(name);
+        if (accountByName != null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteAccount(int id){
+        accountDao.deleteAccount(id);
+    }
+
+    public void updateAccount(Account account){
+        accountDao.updateAccount(account);
+    }
+
+    public Account getAccount(int id) {
+        Account account = accountDao.getAccountById(id);
+        return account;
+    }
+
+    public void setAccountDao(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
 }
